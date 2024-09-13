1a7bd26f42ae743217cd7ec04163d2ec58128102
yyhago <yhago.felipe.teles@gmail.com>
2024-09-12 22:32:40 -0300

    Code commented

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,11 +2,15 @@ import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import './Styles.css';
 
+// Conectando ao servidor Socket.IO na porta 3002
 const socket = io('http://localhost:3002', { withCredentials: true });
 
 const ChatComponent: React.FC = () => {
+  // Estado para armazenar as mensagens do chat
   const [messages, setMessages] = useState<{ text: string; sentByUser: boolean }[]>([]);
+  // Estado para armazenar a mensagem de entrada do usuário
   const [inputMessage, setInputMessage] = useState<string>('');
+  // Referência para o elemento da caixa de chat, usada para rolar automaticamente para baixo
   const chatBoxRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -16,22 +20,24 @@ const ChatComponent: React.FC = () => {
       setMessages(JSON.parse(savedMessages));
     }
 
-    // Recebe mensagens do servidor e atualiza o estado
+    // Configura o evento para receber mensagens do servidor
     socket.on('message', (message: { text: string; sentByUser: boolean }) => {
       setMessages((prevMessages) => {
         const newMessages = [...prevMessages, { ...message, sentByUser: false }];
+        // Salva as mensagens no localStorage para persistência
         localStorage.setItem('chatMessages', JSON.stringify(newMessages));
         return newMessages;
       });
     });
 
+    // Limpa o evento quando o componente desmontar
     return () => {
       socket.off('message');
     };
   }, []);
 
   useEffect(() => {
-    // Rolar para o final do chat quando novas mensagens são adicionadas
+    // Rola para o final do chat quando novas mensagens são adicionadas
     if (chatBoxRef.current) {
       chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
     }
@@ -39,13 +45,17 @@ const ChatComponent: React.FC = () => {
 
   const handleSendMessage = () => {
     if (inputMessage.trim()) {
+      // Cria a nova mensagem
       const message = { text: inputMessage, sentByUser: true };
+      // Envia a mensagem para o servidor
       socket.emit('message', message);
       setMessages((prevMessages) => {
         const newMessages = [...prevMessages, message];
+        // Atualiza o localStorage com a nova lista de mensagens
         localStorage.setItem('chatMessages', JSON.stringify(newMessages));
         return newMessages;
       });
+      // Limpa o campo de entrada após enviar a mensagem
       setInputMessage('');
     }
   };
